6f60405743edd460c11f79aa2b74531b021ce7c9
Heeseo <ransom722@naver.com>
2023-05-18 14:38:40 +0900

    image uploads and modify the route

--- workspace/boo_pygame.py
+++ workspace/boo_pygame.py
@@ -15,14 +15,14 @@ pygame.display.set_caption("BOO GAME")
 clock = pygame.time.Clock()
 
 # 배경(봄, 여름, 가을, 겨울) 설정
-background_spring = pygame.image.load("C:/Users/ranso/OneDrive/바탕 화면/team/team_project/images/background_spring.png") #봄 배경
-background_summer = pygame.image.load("C:/Users/ranso/OneDrive/바탕 화면/team/team_project/images/background_summer.png") #여름 배경
-background_autumn = pygame.image.load("C:/Users/ranso/OneDrive/바탕 화면/team/team_project/images/background_autumn.png") #가을 배경
-background_winter = pygame.image.load("C:/Users/ranso/OneDrive/바탕 화면/team/team_project/images/background_winter.png") #겨울 배경
+background_spring = pygame.image.load("C:/Users/ranso/team/team_project/images/background_spring.png") #봄 배경
+background_summer = pygame.image.load("C:/Users/ranso/team/team_project/images/background_summer.png") #여름 배경
+background_autumn = pygame.image.load("C:/Users/ranso/team/team_project/images/background_autumn.png") #가을 배경
+background_winter = pygame.image.load("C:/Users/ranso/team/team_project/images/background_winter.png") #겨울 배경
 #파일경로 수정, 배경이미지 수정 요망
 
 # 캐릭터(봄, 여름, 가을, 겨울) 설정
-BOO = pygame.image.load("C:/Users/ranso/OneDrive/바탕 화면/team/team_project/images/BOO.png")
+BOO = pygame.image.load("C:/Users/ranso/team/team_project/images/BOO.png")
 #파일경로 수정, 캐릭터 이미지 수정 및 추가 요망 
 BOO_size = BOO.get_rect().size
 BOO_width = BOO_size[0] 
